0646fb9084333dda7fb4f6f196e13fe8b0d20da5
OrangeM21 <orangemtony@gmail.com>
2020-02-07 23:22:00 +0800

    [update]完成第一版本

--- src/main/index.js
+++ src/main/index.js
@@ -22,7 +22,9 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     height: 563,
     useContentSize: true,
-    width: 1000
+    width: 1000,
+    minWidth: 1000,
+    minHeight: 563
   })
 
   mainWindow.loadURL(winURL)
